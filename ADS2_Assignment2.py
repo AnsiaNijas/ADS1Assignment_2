dff8ef2a394d4e2a764a2c9651532d875cfa37e7
Ansia Nijas <35911053+AnsiaNijas@users.noreply.github.com>
2023-04-01 10:38:09 +0100

    Added barplots and line plots methods

--- ADS2_Assignment2.py
+++ ADS2_Assignment2.py
@@ -15,56 +15,91 @@ import numpy as np
 from scipy import stats
 import seaborn as sns
 
+
 def readBarPlotData(filename):
-    df_Arable_Land=pd.read_excel(filename)
-    df_Arable_Land.columns=df_Arable_Land.iloc[2].astype(str)
-    df_Arable_Land_year_as_columns=df_Arable_Land.iloc[170:182,0:].fillna(0)
-    df_Arable_Land_updated= pd.DataFrame.transpose(df_Arable_Land)
-    df_Arable_Land_countries_as_columns=df_Arable_Land_updated.iloc[4:]
-    df_Arable_Land_countries_as_columns.columns=df_Arable_Land_updated.iloc[0].astype(str)
-    df_Arable_Land_countries_as_columns=df_Arable_Land_countries_as_columns.rename(columns={'Country Name':'Year'})
-    # print(df_Arable_Land_year_as_columns.groupby('Country Name').describe())
+    df_Arable_Land = pd.read_excel(filename)
+    df_Arable_Land.columns = df_Arable_Land.iloc[2].astype(str)
+    df_Arable_Land_year_as_columns = df_Arable_Land.iloc[170:182, 0:].fillna(0)
+    df_Arable_Land_updated = pd.DataFrame.transpose(df_Arable_Land)
+    df_Arable_Land_countries_as_columns = df_Arable_Land_updated.iloc[4:]
+    df_Arable_Land_countries_as_columns.columns = df_Arable_Land_updated.iloc[0].astype(
+        str)
+    df_Arable_Land_countries_as_columns = df_Arable_Land_countries_as_columns.rename(
+        columns={'Country Name': 'Year'})
+    df_Arable_Land_countries_as_columns=df_Arable_Land_countries_as_columns.set_index('Year')
+    df_Arable_Land_countries_as_columns=df_Arable_Land_countries_as_columns.iloc[51:61,128:137]
+    df_Arable_Land_countries_as_columns=df_Arable_Land_countries_as_columns.reset_index(drop=False)
     return df_Arable_Land_year_as_columns, df_Arable_Land_countries_as_columns
 
-   
+
 def readHeatData(filename1):
-    df_World_Development_Indicators=pd.read_excel(filename1)
-    df_World_Development_Indicators_updated=df_World_Development_Indicators.iloc[532:539,0:]
-    df_World_Development_Indicators_updated=df_World_Development_Indicators_updated.drop(['Country Name','Country Code','Series Code'],axis=1)
-    df_World_Development_Indicators_updated=pd.DataFrame.transpose(df_World_Development_Indicators_updated)
-    df_World_Development_Indicators_updated.columns=df_World_Development_Indicators_updated.iloc[0]
-    df_World_Development_Indicators_updated.reset_index(drop=True, inplace=True)
-    df_World_Development_Indicators_updated = df_World_Development_Indicators_updated.drop(labels=0, axis=0)
-    df_World_Development_Indicators_updated=df_World_Development_Indicators_updated.fillna(0)
+    df_World_Development_Indicators = pd.read_excel(filename1)
+    df_World_Development_Indicators_updated = df_World_Development_Indicators.iloc[
+        532:539, 0:]
+    df_World_Development_Indicators_updated = df_World_Development_Indicators_updated.drop(
+        ['Country Name', 'Country Code', 'Series Code'], axis=1)
+    df_World_Development_Indicators_updated = pd.DataFrame.transpose(
+        df_World_Development_Indicators_updated)
+    df_World_Development_Indicators_updated.columns = df_World_Development_Indicators_updated.iloc[
+        0]
+    df_World_Development_Indicators_updated.reset_index(
+        drop=True, inplace=True)
+    df_World_Development_Indicators_updated = df_World_Development_Indicators_updated.drop(
+        labels=0, axis=0)
+    df_World_Development_Indicators_updated = df_World_Development_Indicators_updated.fillna(
+        0)
     return df_World_Development_Indicators_updated
 
 
-def barplot1()   :
-    df1,df2=readBarPlotData("Arable_Land.xls")
-    legends_years = ['2020','2019','2018','2017','2016','2015']
+def barplot(filename,title,ylim,figure_name):
+    df1, df2 = readBarPlotData(filename)
+    legends_years = ['2020', '2019', '2018', '2017', '2016', '2015']
     plt.figure(dpi=300)
-    df1.plot.bar(x='Country Name', y=['2020.0','2019.0','2018.0','2017.0','2016.0','2015.0'],rot=90,figsize=(25,20),title='Arable Land',
-    xlabel="Country Name",ylabel="Years", fontsize='large', label=legends_years);
-    plt.ylim(0, 40)
-    plt.legend(title='Years',fontsize=20) 
-    plt.savefig('BarPlot.jpg')
+    df1.plot.bar(x='Country Name', y=['2020.0', '2019.0', '2018.0', '2017.0', '2016.0', '2015.0'], rot=90, figsize=(30, 25), 
+                   fontsize=25, label=legends_years)
+    plt.title(title,fontsize=25)
+    plt.xlabel("Country Name",fontsize=25)
+    plt.ylim(0, ylim)
+    plt.legend(title='Years', fontsize=25)
+    plt.savefig(figure_name)
     plt.show()
     return
+
+
+def lineplot(filename,title,ylim,figure_name):
+    df1, df2 = readBarPlotData(filename)
+    countries = ['Korea, Rep.', 'Kuwait', 'Latin America & Caribbean (excluding high income)',	'Lao PDR',	'Lebanon',	'Liberia',	'Libya',	'St. Lucia',	'Latin America & Caribbean'
+]
+    plt.figure(dpi=300)
+    for i in range(len(countries)):
+        plt.plot(df2['Year'],df2[countries[i]],label=countries[i])    
     
-def heatMap1()   : 
-    
-    df1=readHeatData("World_Development_Indicators.xlsx")
-    corr=df1.corr(method='pearson', min_periods=1)
-    sns.heatmap(data=corr,annot=True)
-    labels=['Net forest depletion','Agricultural land','Agricultural irrigated land','Arable land Agriculture_ forestry_fishing','Arable land','Forest area']
+    plt.title(title,fontsize=5)
+    plt.xlabel("Years",fontsize=5)
+    plt.ylim(0, ylim)
+    plt.legend(fontsize=5)
+    plt.savefig(figure_name)
+    plt.show()
+    return
+
+def heatMap1():
+    df1 = readHeatData("World_Development_Indicators.xlsx")
+    corr = df1.corr(method='pearson', min_periods=1)
+    sns.heatmap(data=corr, annot=True)
+    labels = ['Net forest depletion', 'Agricultural land', 'Agricultural irrigated land',
+              'Agriculture_ forestry_fishing', 'Arable land', 'Forest area']
     plt.title('Greece')
-    plt.xticks([0,1,2,3,4,5],labels)
-    plt.yticks([0,1,2,3,4,5],labels)
-    plt.savefig('HeatMap_Greece.jpg',dpi=300,bbox_inches='tight')
+    plt.xlabel('')
+    plt.ylabel('')
+    plt.xticks([0, 1, 2, 3, 4, 5], labels)
+    plt.yticks([0, 1, 2, 3, 4, 5], labels)
+    plt.savefig('HeatMap_Greece.jpg', dpi=300, bbox_inches='tight')
     plt.show()
     return
-    
+
+
 if __name__ == "__main__":
-    barplot1()
+    barplot("Arable_Land.xls",'Arable Land',40,'BarPlot1.jpg')
+    barplot("Agricultural_Land.xls",'Agricultural Land',80,'BarPlot2.jpg')
+    lineplot('Forest_Area.xls','Forest_Area',85,'LinePlot1.jpg')
     heatMap1()
-    
