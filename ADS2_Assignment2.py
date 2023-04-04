bacc4a12d80eb53fd904415c5e68307631267873
Ansia Nijas <35911053+AnsiaNijas@users.noreply.github.com>
2023-04-04 19:18:59 +0100

    Updated heatmap function with mathplot

--- ADS2_Assignment2.py
+++ ADS2_Assignment2.py
@@ -8,7 +8,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import stats
-import seaborn as sns
 
 
 def read_Bar_Line_Plot_Data(filename):
@@ -17,22 +16,26 @@ def read_Bar_Line_Plot_Data(filename):
     countries as columns.
     """
     df_Arable_Land = pd.read_excel(filename)
+    countries = ['Ethiopia', 'Ghana', 'Greece', 'Guinea',
+                 'India', 'Malaysia', 'Myanmar', 'Uganda']
+    years = ['Country Name', '1990.0', '1995.0',
+             '2000.0', '2005.0', '2010.0', '2015.0', '2020.0']
     df_Arable_Land.columns = df_Arable_Land.iloc[2].astype(str)
-    df_Arable_Land_year_as_columns = df_Arable_Land.iloc[170:182, 0:].fillna(0)
-    df_Arable_Land_updated = pd.DataFrame.transpose(df_Arable_Land)
-    df_Arable_Land_countries_as_columns = df_Arable_Land_updated.iloc[4:]
-    df_Arable_Land_countries_as_columns.columns = df_Arable_Land_updated.iloc[0].astype(
+    df_Arable_Land_year_as_columns = df_Arable_Land.iloc[3:, 0:].fillna(0)
+    df_Arable_Land_year_as_columns = df_Arable_Land_year_as_columns.drop(
+        ['Country Code', 'Indicator Name', 'Indicator Code'], axis=1)
+    df_Arable_Land_year_as_columns = df_Arable_Land_year_as_columns.set_index(
+        'Country Name')
+    df_Arable_Land_year_as_columns = df_Arable_Land_year_as_columns.loc[countries]
+    df_Arable_Land_year_as_columns.reset_index(drop=False, inplace=True)
+    df_Arable_Land_year_as_columns = df_Arable_Land_year_as_columns.loc[:, years]
+    df_Arable_Land_countries_as_columns = pd.DataFrame.transpose(
+        df_Arable_Land_year_as_columns)
+    df_Arable_Land_countries_as_columns.columns = df_Arable_Land_countries_as_columns.iloc[0].astype(
         str)
-    df_Arable_Land_countries_as_columns = df_Arable_Land_countries_as_columns.rename(
-        columns={'Country Name': 'Year'})
-    df_Arable_Land_countries_as_columns = df_Arable_Land_countries_as_columns.set_index(
-        'Year')
-    df_Arable_Land_countries_as_columns = df_Arable_Land_countries_as_columns.iloc[
-        51:61, 0:]
-    df_Arable_Land_countries_as_columns = df_Arable_Land_countries_as_columns[[
-        'Ethiopia', 'Ghana', 'Guinea', 'Madagascar', 'Malawi', 'Rwanda', 'Sierra Leone', 'Uganda']]
-    df_Arable_Land_countries_as_columns = df_Arable_Land_countries_as_columns.reset_index(
-        drop=False)
+    df_Arable_Land_countries_as_columns = df_Arable_Land_countries_as_columns.iloc[1:, 0:]
+    df_Arable_Land_countries_as_columns.reset_index(drop=False, inplace=True)
+    df_Arable_Land_countries_as_columns.columns.values[0] = "Years"
     return df_Arable_Land_year_as_columns, df_Arable_Land_countries_as_columns
 
 
@@ -78,9 +81,9 @@ def barplot(filename, title, ylim, figure_name):
     Arguments: Excel filename, title of barplot, y limits, Figure name 
     """
     df1, df2 = read_Bar_Line_Plot_Data(filename)
-    legends_years = ['2020', '2019', '2018', '2017', '2016', '2015']
+    legends_years = ['1990', '1995', '2000', '2005', '2010', '2015', '2020']
     plt.figure(dpi=300)
-    df1.plot.bar(x='Country Name', y=['2020.0', '2019.0', '2018.0', '2017.0', '2016.0', '2015.0'], rot=90, figsize=(30, 25),
+    df1.plot.bar(x='Country Name', y=['1990.0', '1995.0', '2000.0', '2005.0', '2010.0', '2015.0', '2020.0'], rot=90, figsize=(30, 25),
                    fontsize=25, label=legends_years)
     plt.title(title, fontsize=30)
     plt.xlabel("Country Name", fontsize=30)
@@ -98,18 +101,21 @@ def lineplot(filename, title, y_label, ylimin, ylimax, figure_name):
     Arguments: Excel filename, title of lineplots, y label, start value of ylim, End value of ylim and Figure name 
     """
     df1, df2 = read_Bar_Line_Plot_Data(filename)
-    countries = [['Ethiopia', 'Ghana', 'Guinea', 'Madagascar', 'Malawi', 'Rwanda', 'Sierra Leone', 'Uganda']
-                 ]
+    countries = [['Ethiopia', 'Ghana', 'Greece', 'Guinea',
+                  'India', 'Malaysia', 'Myanmar', 'Uganda']]
+    year_xtick_o = ['1990.0', '1995.0', '2000.0',
+                    '2005.0', '2010.0', '2015.0', '2020.0']
+    year_xtick_n = ['1990', '1995', '2000', '2005', '2010', '2015', '2020']
     plt.figure(dpi=300)
     for i in range(len(countries)):
-        plt.plot(df2['Year'], df2[countries[i]], label=countries[i])
+        plt.plot(df2['Years'], df2[countries[i]], label=countries[i])
     plt.title(title, fontsize=8)
-    plt.xlabel("Years", fontsize=8)
+    plt.xlabel('Years', fontsize=8)
     plt.ylabel(y_label, fontsize=8)
-    plt.xticks(fontsize=8)
-    plt.yticks(fontsize=8)
-    plt.xlim(2012, 2020)
+    plt.xlim('1990.0', '2020.0')
     plt.ylim(ylimin, ylimax)
+    plt.xticks(year_xtick_o, year_xtick_n, fontsize=8)
+    plt.yticks(fontsize=8)
     plt.legend(title='Country Names', title_fontsize=4,
                fontsize=4, loc='upper right')
     plt.savefig(figure_name)
@@ -117,25 +123,32 @@ def lineplot(filename, title, y_label, ylimin, ylimax, figure_name):
     return
 
 
-def heatMap1(filename):
+def heatMap(filename):
     """ 
     Function to compute correlation between different climate change indicators, to plot heatmap and save figure.
     Arguments: Excel filename. 
     """
     df1 = read_Heat_Map_Data(filename)
-    labels = ['Net forest depletion', 'Agricultural land', 'Agricultural irrigated land',
-              'Agriculture_ forestry_fishing', 'Arable land', 'Forest area']
-    corr = df1.corr(method='pearson', min_periods=1)
-    plt.figure()
-    sns.heatmap(data=corr, annot=True, cbar_kws={
-                "label": "Correlation of Climate Change Indicators"})
-    plt.title('Greece')
-    plt.xlabel('')
-    plt.ylabel('')
-    plt.xticks([.5, 1.5, 2.5, 3.5, 4.5, 5.5], labels, ha='center')
-    plt.yticks([.5, 1.5, 2.5, 3.5, 4.5, 5.5], labels, va='center')
-    plt.savefig('HeatMap_Greece.jpg', dpi=300, bbox_inches='tight')
+    labels1 = ['Net forest depletion', 'Agricultural land', 'Agricultural irrigated land',
+               'Agriculture_ forestry_fishing', 'Arable land', 'Forest area']
+    corr = df1.corr(method='pearson', min_periods=1).round(1)
+    fig, ax = plt.subplots(figsize=(8, 6))
+    im = ax.imshow(corr, cmap="Blues")
+    cbar = ax.figure.colorbar(im, ax=ax)
+    cbar.ax.set_ylabel(
+        "Correlation of Climate Change Indicators", rotation=-90, va="bottom")
+    ax.set_xticks(np.arange(len(labels1)), labels=labels1, fontsize=10)
+    ax.set_yticks(np.arange(len(labels1)), labels=labels1, fontsize=10)
+    plt.setp(ax.get_xticklabels(), rotation=-90, ha="left",
+             rotation_mode="anchor")
+    for i in range(len(labels1)):
+        for j in range(len(labels1)):
+            text = ax.text(
+                j, i, corr.iloc[i, j], ha="center", va="center", color="orange", fontsize=10)
+    ax.set_title("Greece")
+    fig.tight_layout()
     plt.show()
+    fig.savefig('HeatMap_Greece.jpg')
     return
 
 
@@ -167,20 +180,21 @@ def stat_analysis_CO2_emission(filename):
     print(df_stats.groupby('Country Name')['Skew'].describe())
     print(df_stats)
     df_stats.to_csv('Statical_Analysis of CO2 Emission.csv')
+    return
 
 
 if __name__ == "__main__":
     #calling function to visualize BarPlot by passing arguments as Excel filename, title of barplot, y limits, Figure name
-    barplot("Arable_Land.xls", 'Arable Land', 40, 'BarPlot1.jpg')
+    barplot("Arable_Land.xls", 'Arable Land', 50, 'BarPlot1.jpg')
     #calling function to visualize BarPlot by passing arguments as Excel filename, title of barplot, y limits, Figure name
     barplot("Agricultural_Land.xls", 'Agricultural Land', 80, 'BarPlot2.jpg')
     #calling function to visualize LinePlot by passing arguments as Excel filename, title of lineplots, y label, start value of ylim, End value of ylim and Figure name
     lineplot('Forest_Area.xls', 'Forest_Area',
-             'Land area (% Percentage)', 10, 45, 'LinePlot1.jpg')
+             'Land area (% Percentage)', 10, 80, 'LinePlot1.jpg')
     #calling function to visualize LinePlot by passing arguments as Excel filename, title of lineplots, y label, start value of ylim, End value of ylim and Figure name
     lineplot('Forest Depletion.xls', 'Forest Depletion',
-             'Net forest depletion (% Percentage)', 3, 15, 'LinePlot2.jpg')
+             'Net forest depletion (% Percentage)', -5, 35, 'LinePlot2.jpg')
     #calling function to visualize HeatMap by passing arguments as Excel filename
-    heatMap1("World_Development_Indicators.xlsx")
+    heatMap("World_Development_Indicators.xlsx")
     #calling function to compute and analyse statical data of CO2 Emission by passing arguments as Excel filename
     stat_analysis_CO2_emission('CO2_emission_by_liquids_solids_fuels.xlsx')
